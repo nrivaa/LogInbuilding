e9f12fc870fc7a66726c32de8d3580fcb49fa193
nrivaa <nri.vaa@gmail.com>
2015-08-19 10:31:13 +0700

    Create Project

--- app/src/main/java/com/nqm/loginbuilding/CreateProjectActivity.java
+++ app/src/main/java/com/nqm/loginbuilding/CreateProjectActivity.java
@@ -2,17 +2,39 @@ package com.nqm.loginbuilding;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import android.net.Uri;
+import android.location.Criteria;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.Toast;
 
 
 public class CreateProjectActivity extends ActionBarActivity {
@@ -21,6 +43,18 @@ public class CreateProjectActivity extends ActionBarActivity {
     private TextView tvBuildingID,tvName,tvContact,tvNumber,tvImsiA,tvImsiD,tvImsiT,tvImsi3,tvAddress,tvTambon,tvDistrict,tvPostCode,tvDetail;
     private ImageButton btnCapture;
     private RadioGroup rgService, rgAccess;
+
+    // Location loc ;
+    LocationManager lm;
+    String latitude;
+    String longitude;
+    // Activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    // directory name to store captured images and videos
+    private static final String IMAGE_DIRECTORY_NAME = "Transtagram Camera";
+    private Uri fileUri; // file URL to store image/video
+    public List<String> listdata = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -114,7 +148,33 @@ public class CreateProjectActivity extends ActionBarActivity {
 
         tvBuildingID = (TextView)findViewById(R.id.createProject_BuildingID);
         tvName = (TextView)findViewById(R.id.createProject_BuildingName);
-        tvNumber = (TextView)findViewById(R.id.createProject_contactName);
+        tvContact = (TextView)findViewById(R.id.createProject_contactName);
+        tvNumber = (TextView)findViewById(R.id.createProject_contactNumber);
+        tvImsiA = (TextView)findViewById(R.id.createProject_imei_awn);
+        tvImsiD = (TextView)findViewById(R.id.createProject_imei_dtac);
+        tvImsiT = (TextView)findViewById(R.id.createProject_imei_trueh);
+        tvImsi3 = (TextView)findViewById(R.id.createProject_imei_3bb);
+        tvAddress = (TextView)findViewById(R.id.createProject_address);
+        tvTambon = (TextView)findViewById(R.id.createProject_tambon);
+        tvDistrict = (TextView)findViewById(R.id.createProject_district);
+        tvPostCode = (TextView)findViewById(R.id.createProject_postcode);
+        tvDetail = (TextView)findViewById(R.id.createProject_buildingDetail);
+        btnCapture = (ImageButton)findViewById(R.id.createProject_imgbtn);
+        rgService = (RadioGroup)findViewById(R.id.rbService);
+        rgAccess = (RadioGroup)findViewById(R.id.rbAccess);
+
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                captureImage();
+            }
+        });
 
         Button btn_submit = (Button)findViewById(R.id.projectCreate_btn_submit);
         btn_submit.setOnClickListener(new View.OnClickListener() {
@@ -124,6 +184,154 @@ public class CreateProjectActivity extends ActionBarActivity {
                 startActivity(i);
             }
         });
+
+        // Checking camera availability
+        if (!isDeviceSupportCamera()) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! Your device doesn't support camera",
+                    Toast.LENGTH_LONG).show();
+            // will close the app if the device does't have camera
+            finish();
+        }
+    }
+
+    // SubmitProject
+    private void SubmitProject(boolean submitFlag) {
+
+
+        //Upload to Async
+
+    }
+
+    /*
+	 * Capturing Camera Image will launch camera app-request image capture
+	 */
+    private void captureImage() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+        // start the image capture Intent
+        startActivityForResult(intent, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+
+        //Get lat,long
+        GPSTracker  gps = new GPSTracker(CreateProjectActivity.this);
+        // check if GPS enabled
+        if(gps.canGetLocation()){
+            latitude = String.valueOf(gps.getLatitude());
+            longitude = String.valueOf(gps.getLongitude());
+            // \n is for new line
+            //Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        }else{
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
+        Log.e("gps :","Lat="+latitude+",Lng="+longitude);
+
+
+    }
+    /*
+	 * Creating file uri to store image/video
+	 */
+    public Uri getOutputMediaFileUri(int type) {
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+    /*
+	 * returning image / video
+	 */
+    private static File getOutputMediaFile(int type) {
+
+        // External sdcard location
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+                        + IMAGE_DIRECTORY_NAME + " directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                    + "IMG_" + timeStamp + ".jpg");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    /**
+     * Receiving activity result method will be called after closing the camera
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // successfully captured the image
+                // display it in image view
+                previewCapturedImage();
+            } else if (resultCode == RESULT_CANCELED) {
+                // user cancelled Image capture
+                Toast.makeText(getApplicationContext(),
+                        "User cancelled image capture", Toast.LENGTH_SHORT)
+                        .show();
+            } else {
+                // failed to capture image
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! Failed to capture image", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+
+    /*
+	 * Display image from a path to ImageView
+	 */
+    private void previewCapturedImage() {
+        try {
+
+            // bimatp factory
+            BitmapFactory.Options options = new BitmapFactory.Options();
+
+            // downsizing image as it throws OutOfMemory Exception for larger
+            // images
+            options.inSampleSize = 8;
+
+            final Bitmap bitmap = BitmapFactory.decodeFile(fileUri.getPath(),
+                    options);
+
+            btnCapture.setImageBitmap(bitmap);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Checking device has camera hardware or not
+     * */
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
     }
 
     @Override
